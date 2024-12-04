5957da953fc9e89adb13ed1ec8c1f92d2adbd2c8
initinll <initinli@yahoo.com>
2024-12-04 17:36:06 +0530

    Initial project

---
+++ ExpressionTrees/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+
+var xExpression = Expression.Parameter(typeof(int), "x");
+var constant12Expression = Expression.Constant(12);
+var constant4Expression = Expression.Constant(4);
+
+var greaterThan = Expression.GreaterThanOrEqual(xExpression, constant12Expression);
+var lessThan = Expression.LessThanOrEqual(xExpression, constant4Expression);
+
+// Expression => x > 12 or x < 4
+var orExp = Expression.Or(lessThan, greaterThan);
+// Expression => x > 12
+var expr1 = Expression.Lambda<Func<int, bool>>(body: greaterThan, parameters: [xExpression]);
+var func1 = expr1.Compile();
+
+var expr2 = Expression.Lambda<Func<int, bool>>(body: orExp, parameters: [xExpression]);
+var func2 = expr2.Compile();
+
+Console.WriteLine(func1(11));
+Console.WriteLine(func2(10));
+
+
